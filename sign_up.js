cdd2b3ad70e5550d45fc61a9ec5052aab6384f3b
polina <polina.zdanovich.02@mail.ru>
2022-10-11 10:56:26 +0300

    edits

--- sign_up.js
+++ sign_up.js
@@ -4,21 +4,37 @@ const conf_passw = document.querySelector(".confirm-psw .input")
 const btn = document.querySelector(".button .btn")
 const info = document.querySelector(".email .vector")
 
-function checkEmail(input) {
-    if (!/([\w\.-]+@[a-z]+\.[a-z]{1,3})|([\d]{3}[\d]{2}[\d]{7})|(\+[\d]{3}[\d]{2}[\d]{7})/g.test(input)) throw new Error("Incorrect format")
-    if (!input) throw new Error("Input is empty")
-    return true
+function checkEmail(mail) {
+    try{
+        if (!/([\w\.-]+@[a-z]+\.[a-z]{1,3})|([\d]{3}[\d]{2}[\d]{7})|(\+[\d]{3}[\d]{2}[\d]{7})/g.test(mail)) throw new Error("Incorrect format")
+        if (!mail) throw new Error("Input is empty")
+        email.style = "color: green"
+    } catch (error) {
+        email.value = error.message
+        email.style = "color: red"
+    }
 }
 
-function checkPassword(input) {
-    if (!/[\wа-яА-Я\-\/+&$%#!\.\\\/\|]{8,}/g.test(input)) throw new Error("Password must contain at least 8 characters")
-    if (!input) throw new Error("Input is empty")
-    return true
+function checkPassword(password) {
+    try {
+        if (!/[\wа-яА-Я\-\/+&$%#!\.\\\/\|]{8,}/g.test(password)) throw new Error("Password must contain at least 8 characters")
+        if (!password) throw new Error("Input is empty")
+        passw.style = "color: green"
+    } catch (error) {
+        passw.value = error.message
+        passw.style = "color: red"
+    }
 }
 
 function checkConfirmPassword(password, confirmPassword) {
-    if (password !== confirmPassword) throw new Error("Incorret password")
-    if (!password) throw new Error("Input is empty")
+    try {
+        if (password !== confirmPassword) throw new Error("Incorret password")
+        if (!password) throw new Error("Input is empty")
+        conf_passw.style = "color: green"
+    } catch (error) {
+        conf_passw.value = error.message
+        conf_passw.style = "color: red"
+    }
 }
 
 email.addEventListener("click", () => {
@@ -38,29 +54,29 @@ conf_passw.addEventListener("click", () => {
 })
 
 btn.addEventListener("click", () => {
-    try {
-        checkEmail(email.value)
-        email.style = "color: green"
-    } catch (error) {
-        email.value = error.message
-        email.style = "color: red"
-    }
+    // try {
+    //     checkEmail(email.value)
+    //     email.style = "color: green"
+    // } catch (error) {
+    //     email.value = error.message
+    //     email.style = "color: red"
+    // }
 
-    try {
-        checkPassword(passw.value)
-        passw.style = "color: green"
-    } catch (error) {
-        passw.value = error.message
-        passw.style = "color: red"
-    }
+    // try {
+    //     checkPassword(passw.value)
+    //     passw.style = "color: green"
+    // } catch (error) {
+    //     passw.value = error.message
+    //     passw.style = "color: red"
+    // }
 
-    try {
-        checkConfirmPassword(conf_passw.value, passw.value)
-        conf_passw.style = "color: green"
-    } catch (error) {
-        conf_passw.value = error.message
-        conf_passw.style = "color: red"
-    }
+    // try {
+    //     checkConfirmPassword(conf_passw.value, passw.value)
+    //     conf_passw.style = "color: green"
+    // } catch (error) {
+    //     conf_passw.value = error.message
+    //     conf_passw.style = "color: red"
+    // }
 
     try {
         checkEmail(email.value)
